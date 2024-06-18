30239d1aa8dabd1e0e395e212ce55060283681e0
Vishal Panwar <vishal4panwar@gmail.com>
2024-06-18 15:25:31 +0530

    03-suggested user data is fetched

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton.jsx";
-import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy.js";
+// import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy.js";
+
+import { useQuery } from "@tanstack/react-query";
 
 const RightPanel = () => {
-  const isLoading = false;
+  // const isLoading = false;
 
+  const {
+    data: suggestedUsers,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/user/suggested");
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Failed to load users");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  if (suggestedUsers?.length === 0) {
+    return <div className=" md:w-64 w-0"> </div>;
+  }
   return (
     <div className="hidden lg:block my-4 mx-2">
       <div className="bg-[#16181C] p-4 rounded-md sticky top-2">
@@ -20,7 +44,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4"
